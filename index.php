7a10622a7b3797c19bde9058cbdc9f7fc448523a
manh612001 <nguyendacmanh2001@gmail.com>
2021-11-05 00:51:03 +0700

    update 4

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     require_once('./database/dbhelper.php');
     $sql = "select * from category ";
     $data =  executeResult($sql);
-    $sql1= "select * from tour limit 4";
+    $sql1= "select * from tour limit 6";
     $data1 = executeResult($sql1);
     function Url($thumbnail, $Url = "./") { // thêm đường dẫn folder $Url
 
@@ -36,6 +36,9 @@
     height: 550px;
 }
 
+.card{
+  width: 98%;
+}
 .col-md-3 img{
     width: 200px;
     height: 200px;
@@ -49,11 +52,14 @@ form{
     top: -150px;
     z-index: 1;
 }
+nav{
+  background:#dfe6e9;
+}
     </style>
   </head>
   <body>
     <header>
-      <nav class="navbar navbar-expand-lg bg-info navbar-dark fixed-top" >
+      <nav class="navbar navbar-expand-lg  navbar-dark fixed-top" >
         <!-- Brand -->
         <a class="navbar-brand" href="#"><h3>BlackWhite</h2></a>
       
@@ -101,7 +107,7 @@ form{
             <img src="./upload/pexels-foc-foodoncam-58597.jpg" alt="Chicago">
           </div>
           <div class="carousel-item">
-            <img src="./upload/hinh-anh-cute-de-thuong.jpg" alt="New York">
+            <img src="./upload/pexels-quang-nguyen-vinh-2161449.jpg" alt="New York">
           </div>
         </div>
       
@@ -112,22 +118,28 @@ form{
         <a class="carousel-control-next" href="#demo" data-slide="next">
           <span class="carousel-control-next-icon"></span>
         </a>
-      <div style="position:relative;top:-150px;">
-        <input type="text" style="padding:5px 15px;" placeholder="Bạn muốn đi đâu?">
-        <a href="#" style="position:relative;right:40px;"><i class="fas fa-search"></i></a>
+      <div style="margin:auto;position:relative;top:-150px;width:30%;">
+        <input type="text" style="padding:5px 15px;"class="form-control" placeholder="Bạn muốn đi đâu?"><a href="#" style="position:absolute;right:15px;top:5px;"><i class="fas fa-search"></i></a></input>
+        
       </div>
       </div>
-      <h3 style="text-align:center; margin-top:20px">Các tour du lịch mới nhất</h3>
+      <h2 style="text-align:center; margin-bottom:20px;">Các tour du lịch mới nhất</h2>
       <div class="container">
         <div class="row">
           <?php
             foreach($data1 as $value){
               echo'
               
-                <div class="col-md-3 col-sm-6 card">
-                  <img src="'.Url($value['thumbnail']).'" style="width:100%"></img>
-                  <p>'.$value['title'].'</p>
-                  <p>'.$value['discount'].'</p>
+                <div class="col-md-4 col-sm-6 " style="margin-bottom:20px;">
+                  <div class="card" style="margin: 0 auto;">
+                  <img src="'.Url($value['thumbnail']).'" style="height:192px;"></img>
+                  <p style="padding:0 15px;margin-top:20px;">Tên: '.$value['title'].'</p>
+                  <p style="padding:0 15px;color:red"><span style="color:black;">Giá:</span> '.number_format($value['discount']).'đ</p>
+                  <div class="bt">
+                    <button class="btn btn-danger w-100" style="margin-bottom:5px;"><i class="far fa-shopping-cart" style="margin-right:15px;"></i>Đặt ngay</button>
+                    <button type="button" class="btn btn-outline-primary w-100" >Xem chi tiết</button>
+                  </div>
+                  </div>
                 </div>
               ';
             }
@@ -135,6 +147,128 @@ form{
         </div>
       </div>
     </main>
+    <!-- Footer -->
+    <footer class="text-center text-white" style="background-color: #caced1;">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Images -->
+    <section class="">
+      <div class="row">
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/113.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/111.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/112.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/114.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/115.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://mdbootstrap.com/img/new/fluid/city/116.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Section: Images -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, .8);">
+    © 2020 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
